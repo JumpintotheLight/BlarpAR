2e7e1c27e55b01b5a31a89852147d2b28777f633
Sky Rolnick <skymeson@gmail.com>
2020-03-11 02:20:08 -0400

    Replaced Mirror version.

---
+++ Assets/NetworkedSingleton/NetworkedSingleton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedSingleton : MonoBehaviour
+{
+
+    public static NetworkedSingleton singleton;
+
+
+    //Set singleton
+    private void Awake()
+    {
+        if (singleton != null)
+        {
+            if (singleton != this)
+            {
+                Debug.Log("nBG Destroyed");
+                GameObject.Destroy(this.gameObject);
+                return;
+            }
+        }
+        singleton = this;
+        GameObject.DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
